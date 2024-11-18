e9e2d7dd81a0b5c0e2d7cf134424c48497198f07
s184361 <s184361@dtu.dk>
2024-11-18 18:35:43 +0100

    new check labels

--- check_labels.py
+++ check_labels.py
@@ -1,154 +1,108 @@
-# %%
 import supervision as sv
 import os
 import numpy as np
 import re
 import yaml
 
-# Print the version of the supervision package
-print("Supervision version:", sv.__version__)
+def print_supervision_version():
+    print("Supervision version:", sv.__version__)
 
-# %%
-HOME = os.getcwd()
-ANNOTATIONS_DIRECTORY_PATH = f"{HOME}/dataset/train/labels"
-IMAGES_DIRECTORY_PATH = f"{HOME}/dataset/train/images"
-DATA_YAML_PATH = f"{HOME}/dataset/data.yaml"
+def load_dataset(images_directory_path, annotations_directory_path, data_yaml_path):
+    return sv.DetectionDataset.from_yolo(
+        images_directory_path=images_directory_path,
+        annotations_directory_path=annotations_directory_path,
+        data_yaml_path=data_yaml_path,
+    )
 
-# GT data
-GT_ANNOTATIONS_DIRECTORY_PATH = f"{HOME}/data/BoudingBoxes"
-GT_IMAGES_DIRECTORY_PATH = f"{HOME}/images"
-GT_DATA_YAML_PATH = f"{HOME}/data/data.yaml"
-# %% Load SAM labels
-# Load the dataset using YOLO format
-dataset = sv.DetectionDataset.from_yolo(
-    images_directory_path=IMAGES_DIRECTORY_PATH,
-    annotations_directory_path=ANNOTATIONS_DIRECTORY_PATH,
-    data_yaml_path=DATA_YAML_PATH,
-)
-# %% Update labels
-# go through all txt files in labels directory
+def update_labels(gt_annotations_directory_path, gt_data_yaml_path):
+    with open(gt_data_yaml_path, "r") as file:
+        data_yaml = yaml.safe_load(file)
+    label_map = {label: idx for idx, label in enumerate(data_yaml["names"])}
+    labels_directory = os.path.join(gt_annotations_directory_path)
+    for filename in os.listdir(labels_directory):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(labels_directory, filename)
+            with open(file_path, "r") as file:
+                lines = file.readlines()
+            new_lines = []
+            for line in lines:
+                parts = line.strip().split()
+                if len(parts) == 5:
+                    label = parts[0].lower().replace("_", " ")
+                    bbox = parts[1:]
+                    label_number = label_map.get(label, -1)
+                    new_line = f"{label_number} " + " ".join(bbox) + "\n"
+                    new_lines.append(new_line)
+            new_filename = re.sub(r"_anno", "", filename)
+            new_file_path = os.path.join(labels_directory, new_filename)
+            with open(new_file_path, "w") as file:
+                file.writelines(new_lines)
 
-# Load the YAML file to get the mapping of labels to numbers
-with open(GT_DATA_YAML_PATH, "r") as file:
-# Load the YAML file
-    data_yaml = yaml.safe_load(file)
-# Create a mapping from label names to their corresponding indices
-label_map = {label: idx for idx, label in enumerate(data_yaml["names"])}
+def compare_classes(gt_dataset, dataset):
+    print("GT classes: ", gt_dataset.classes)
+    print("Dataset classes: ", dataset.classes)
+    gt_classes_not_in_dataset = set(gt_dataset.classes) - set(dataset.classes)
+    print("GT classes not in dataset classes: ", gt_classes_not_in_dataset)
 
-# Directory containing the label files
-labels_directory = os.path.join(GT_ANNOTATIONS_DIRECTORY_PATH)
+def compare_image_keys(gt_dataset, dataset):
+    image_dataset = [os.path.splitext(os.path.basename(path))[0] for path, _, _ in dataset]
+    image_gt_dataset = [os.path.splitext(os.path.basename(path))[0] for path, _, _ in gt_dataset]
+    image_dataset.sort()
+    image_gt_dataset.sort()
+    print("Dataset images: ", image_dataset)
+    print("GT images: ", image_gt_dataset)
+    print("GT images not in dataset images: ", image_gt_dataset == image_dataset)
+    for path, _, _ in dataset:
+        key = os.path.basename(path)
+        if os.path.splitext(key)[0] not in image_gt_dataset:
+            print(f"Key {key} not in GT dataset")
+        else:
+            print(f"Key {key} in GT dataset")
 
-# Iterate over all .txt files in the labels directory
-for filename in os.listdir(labels_directory):
-    if filename.endswith(".txt"):
-        file_path = os.path.join(labels_directory, filename)
+def evaluate_detections(dataset, gt_dataset):
+    for key in dataset.annotations.keys():
+        for i in range(len(dataset.annotations[key])):
+            dataset.annotations[key].confidence = np.ones_like(dataset.annotations[key].class_id)
+    for key in gt_dataset.annotations.keys():
+        for i in range(len(gt_dataset.annotations[key])):
+            gt_dataset.annotations[key].confidence = np.ones_like(gt_dataset.annotations[key].class_id)
+    predictions = []
+    targets = []
+    gt_dict = {os.path.basename(image_path).replace(".png", ".jpg"): annotation for image_path, _, annotation in gt_dataset}
+    for image_path, _, annotation in dataset:
+        key = os.path.basename(image_path)
+        predictions.append(annotation)
+        if key in gt_dict:
+            targets.append(gt_dict[key])
+    confusion_matrix = sv.ConfusionMatrix.from_detections(
+        predictions=predictions,
+        targets=targets,
+        classes=dataset.classes,
+        iou_threshold=0.5
+    )
+    confusion_matrix.plot(normalize=True)
+    print(confusion_matrix)
+    sv.MeanAveragePrecision.from_detections(
+        predictions=predictions,
+        targets=targets
+    )
 
-        # Read the file content
-        with open(file_path, "r") as file:
-            lines = file.readlines()
+def main():
+    HOME = os.getcwd()
+    ANNOTATIONS_DIRECTORY_PATH = f"{HOME}/dataset/train/labels"
+    IMAGES_DIRECTORY_PATH = f"{HOME}/dataset/train/images"
+    DATA_YAML_PATH = f"{HOME}/dataset/data.yaml"
+    GT_ANNOTATIONS_DIRECTORY_PATH = f"{HOME}/data/BoudingBoxes"
+    GT_IMAGES_DIRECTORY_PATH = f"{HOME}/images"
+    GT_DATA_YAML_PATH = f"{HOME}/data/data.yaml"
 
-        # Convert labels to numbers
-        new_lines = []
-        for line in lines:
-            parts = line.strip().split()
-            if len(parts) == 5:
-                label = parts[0].lower().replace("_", " ")
-                bbox = parts[1:]
-                label_number = label_map.get(label, -1)  # Use -1 if label not found
-                new_line = f"{label_number} " + " ".join(bbox) + "\n"
-                new_lines.append(new_line)
+    print_supervision_version()
+    dataset = load_dataset(IMAGES_DIRECTORY_PATH, ANNOTATIONS_DIRECTORY_PATH, DATA_YAML_PATH)
+    update_labels(GT_ANNOTATIONS_DIRECTORY_PATH, GT_DATA_YAML_PATH)
+    gt_dataset = load_dataset(GT_IMAGES_DIRECTORY_PATH, GT_ANNOTATIONS_DIRECTORY_PATH, GT_DATA_YAML_PATH)
+    compare_classes(gt_dataset, dataset)
+    compare_image_keys(gt_dataset, dataset)
+    evaluate_detections(dataset, gt_dataset)
 
-        # Save as txt file without "_anno" in the title
-        # The regex pattern r"_anno" is used to remove the substring "_anno" from the filename
-        new_filename = re.sub(r"_anno", "", filename)
-        new_file_path = os.path.join(labels_directory, new_filename)
-        with open(new_file_path, "w") as file:
-            file.writelines(new_lines)
-
-# %% Load GT_WOOD dataset
-
-gt_dataset = sv.DetectionDataset.from_yolo(
-    images_directory_path=GT_IMAGES_DIRECTORY_PATH,
-    annotations_directory_path=GT_ANNOTATIONS_DIRECTORY_PATH,
-    data_yaml_path=GT_DATA_YAML_PATH,
-)
-# %%
-# compare gt classes with dataset classes
-print("GT classes: ", gt_dataset.classes)
-print("Dataset classes: ", dataset.classes)
-gt_classes_not_in_dataset = set(gt_dataset.classes) - set(dataset.classes)
-print("GT classes not in dataset classes: ", gt_classes_not_in_dataset)
-# %%
-# compare dataset image.keys with gt_dataset image.keys
-print("Dataset image keys: ", dataset.images.keys())
-print("GT image keys: ", gt_dataset.images.keys())
-# print("GT image keys not in dataset image keys: ", gt_dataset.images.keys() == dataset.images.keys())
-
-image_dataset = []
-image_gt_dataset = []
-for path, image, annotation in dataset:
-    image_dataset.append(os.path.splitext(os.path.basename(path))[0])
-for path, image, annotation in gt_dataset:
-    image_gt_dataset.append(os.path.splitext(os.path.basename(path))[0])
-#sort lists
-image_dataset.sort()
-image_gt_dataset.sort()
-print("Dataset images: ", image_dataset)
-print("GT images: ", image_gt_dataset)
-print("GT images not in dataset images: ", image_gt_dataset == image_dataset)
-# %%
-# check if dataset key base name is the same as gt_dataset key base name after removing extension
-for path, image, annotation in dataset:
-    key = os.path.basename(path)
-    if os.path.splitext(key)[0] not in image_gt_dataset:
-        print(f"Key {key} not in GT dataset")
-    else:
-        print(f"Key {key} in GT dataset")
-
-# %% evaluate using from_detections
-
-# assign 1 confidence to all annotations in dataset and gt_dataset
-for key in dataset.annotations.keys():
-    for i in range(len(dataset.annotations[key])):
-        dataset.annotations[key].confidence= np.ones_like(dataset.annotations[key].class_id)
-        #print(dataset.annotations[key].confidence)
-for key in gt_dataset.annotations.keys():
-    for i in range(len(gt_dataset.annotations[key])):
-        gt_dataset.annotations[key].confidence= np.ones_like(gt_dataset.annotations[key].class_id)
-
-# Check if all annotations in dataset have class_id
-for key in dataset.annotations.keys():
-    if not hasattr(dataset.annotations[key], 'class_id'):
-        print(f"Annotation in dataset for {key} does not have class_id")
-
-# Check if all annotations in gt_dataset have class_id
-for key in gt_dataset.annotations.keys():
-    if not hasattr(gt_dataset.annotations[key], 'class_id'):
-        print(f"Annotation in gt_dataset for {key} does not have class_id")
-
-# %%
-predictions = []
-targets = []
-gt_dict = {os.path.basename(image_path).replace(".png", ".jpg"): annotation for image_path, image, annotation in gt_dataset}
-
-for image_path, image, annotation in dataset:
-    key = os.path.basename(image_path)
-    predictions.append(annotation)
-    if key in gt_dict:
-        targets.append(gt_dict[key])
-#%%
-confusion_matrix = sv.ConfusionMatrix.from_detections(
-    predictions=predictions,
-    targets=targets,
-    classes=dataset.classes,
-    iou_threshold=0.5
-)
-# Plot confusion matrix
-confusion_matrix.plot(normalize=True)
-print(confusion_matrix)
-# Calculate the mean average precision (mAP) from the detections
-sv.MeanAveragePrecision.from_detections(
-    predictions=predictions,
-    targets=targets
-)
-# %%
+if __name__ == "__main__":
+    main()
